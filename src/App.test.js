5d67941691dc306975e759f56ba17dcbcf37004d
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2021-09-08 20:35:51 -0300

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { fireEvent, render, screen} from '@testing-library/react';
+
+import App, {calcularNovoSaldo} from './App';
+
+describe('Componente principal', () => {
+    describe('Quando eu abro o app do banco', () => {
+        it('o nome é exibido', () => {
+            render(<App/>)
+            expect(screen.getByText('ByteBank')).toBeInTheDocument();
+        })
+        
+        it('o saldo é exibido', () => {
+            render(<App/>)
+            expect(screen.getByText('Saldo:')).toBeInTheDocument();
+        })
+    
+        it('o botão de transação é exibido', () => {
+            render(<App/>)
+            expect(screen.getByText('Realizar operação'))
+        })
+    })
+    describe('Quando eu realizo uma transação', () => {
+        it('Que é um saque, o valor vai diminuir', () => {
+            const valores = {
+                transacao: 'deposito',
+                valor: 50
+            }
+            const novoSaldo = calcularNovoSaldo(valores, 50)
+
+            expect(novoSaldo).toBe(100)
+
+        })
+        it('que é um saque, a transação deve ser realizada', () => {
+
+            render(<App/>)
+
+            const saldo = screen.getByText('R$ 1000');
+            const transacao = screen.getByLabelText('Saque');
+            const valor = screen.getByTestId('valor');
+            const botaoTransacao =  screen.getByText('Realizar operação');
+
+            
+            fireEvent.click(transacao, {target: {value: 'saque'}});
+            fireEvent.change(valor, {target: {value: 10}});
+            fireEvent.click(botaoTransacao);
+            
+            expect(saldo.textContent).toBe('R$ 990')
+        })
+    })
+})
